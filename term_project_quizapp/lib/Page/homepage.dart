0cd09b81d61ccf54d5eb61aa4de324b59ae29e43
Puttipong Leekeawsai <puttipong.le0695@gmail.com>
2024-03-01 10:13:44 +0700

    Create Home Page

---
+++ term_project_quizapp/lib/Page/homepage.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
